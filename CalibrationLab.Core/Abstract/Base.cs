d97ff5dc085d1b1f9d1d62fdd554bb3521934587
kocserdar <serdkoc@gmail.com>
2023-12-11 23:56:38 +0300

    sec

---
+++ CalibrationLab.Core/Abstract/Base.cs
@@ -0,0 +1,22 @@
+﻿using CalibrationLab.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalibrationLab.Core.Abstract
+{
+    public abstract class Base
+    {
+        public int Id { get; set; }
+        
+        public DateTime CreatedAt { get; set; } 
+        
+        public DateTime LastUpdatedAt { get; set; }
+        
+        public DateTime PassivatedAt { get; set; }
+
+        public Status Status { get; set; }
+    }
+}
---
+++ CalibrationLab.Core/Enums/Status.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalibrationLab.Core.Enums
+{
+    public enum Status
+    {
+        Active,
+        Updated,
+        Passive
+    }
+}
